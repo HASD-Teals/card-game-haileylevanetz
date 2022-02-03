365b6c6816c19014b4a4009a8a7455a324f86a77
haileylevanetz <haileylevanetz@gmail.com>
2022-02-03 11:33:40 -0600

    check

--- src/Deck.java
+++ src/Deck.java
@@ -7,7 +7,34 @@ public class Deck {
             this.cards= new Card[52];
             int counter=0;
             String rank="";
+            for(int i=14; i<28; i++){
+                if(counter >= 2 && counter < 11) {
+                    rank = ""+counter;
+                }
+                if(counter==1){
+                    rank="A";
+                }
+                if(counter==11){
+                    rank="J";
+                }
+                if(counter==12){
+                    rank="Q";
+                }
+                if(counter==13){
+                    rank="K";
+                }
+                Card temp= new Card("red", rank, "\u0003", i, true);
+                if(counter>10){
+                    //temp[i].setValue()=10;
+                }
+                cards[counter]=temp;
+                counter++;
+               
+            }
             for(int i=1; i<14; i++){
+                if(i >= 2 && i < 11) {
+                    rank = ""+i;
+                }
                 if(i==1){
                     rank="A";
                 }
@@ -20,10 +47,7 @@ public class Deck {
                 if(i==13){
                     rank="K";
                 }
-                //else{
-                    //rank= ""+i;
-               // }
-                Card temp= new Card("red", rank, "\u0003", i, true);
+                Card temp= new Card("red", rank, "\u0004", i, true);
                 if(i>10){
                     //temp[i].setValue()=10;
                 }
